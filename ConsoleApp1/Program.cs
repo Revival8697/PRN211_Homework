95479a1792075b586419bb7696eafc9b0e7f0491
sp1cyf0x <ilovecreeper372@gmail.com>
2024-01-06 16:58:30 +0000

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿class Ex1
+{
+    static bool IsPrime(int n)
+    {
+        if (n <= 1) { return false; }
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0) { return false; }
+        }
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] array = new int[10];
+        Console.WriteLine("Enter 10 numbers, one by one:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nPrime numbers in the array:");
+        foreach (int n in array)
+        {
+            if (IsPrime(n)) { Console.WriteLine(n); }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿class Ex2
+{
+    static void Main(string[] args)
+    {
+        double a, b, c = 0;
+        Console.Write("Enter a: ");
+        a = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter b: ");
+        b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nSelect an operation:" +
+            "\n1 for +" +
+            "\n2 for -" +
+            "\n3 for *" +
+            "\n4 for /");
+        string op = Console.ReadLine();
+
+        switch (op)
+        {
+            case "1": c = a + b; break;
+            case "2": c = a - b; break;
+            case "3": c = a * b; break;
+            case "4":
+                if (b == 0) { Console.WriteLine("\nOf course I have a Divide by 0 check, lol."); }
+                else { c = a / 4; break; }
+                return;
+            default:
+                Console.WriteLine("You can only choose from 1, 2, 3 or 4.");
+                return;
+        }
+        Console.WriteLine($"\nResult: {c}");
+    }
+}
