0a37f48d9cdf37b80dc19b5210fa86768bba0b31
Revival8697 <24849056+Revival8697@users.noreply.github.com>
2024-03-13 19:14:34 +0700

    Update database

--- Assignment/Asm_03/DataAccess/FStoreContext.cs
+++ Assignment/Asm_03/DataAccess/FStoreContext.cs
@@ -24,7 +24,7 @@ namespace DataAccess
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=sa;database=PRN211_ASM_02;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-ACECPNJ;uid=sa;pwd=sa;database=Ass3;TrustServerCertificate=True");
             }
         }
 
